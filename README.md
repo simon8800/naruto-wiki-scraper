c9827de3325a37b93766614375411f6705b7ddeb
Simon Mei <smei8800@gmail.com>
2019-08-28 15:16:51 -0400

    Finished scraping character info

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This program scrapes data on all characters from the [Naruto Wiki Page](https://naruto.fandom.com/wiki/Narutopedia).
 
-- Uses [Puppeteer](https://github.com/GoogleChrome/puppeteer) for web scraping
+## TOOLS
+
+- [Puppeteer](https://github.com/GoogleChrome/puppeteer) for automated scraping
--- scrapeCharacters.js
+++ scrapeCharacters.js
@@ -1,54 +1,77 @@
-//  Getting info from each character page
-async function getCharacterInfoFrom(page) {
-  const results = await page.evaluate(() => {
-    let name = document.querySelector('#PageHeader > div.page-header__main > h1').innerText;
-    let summary = document.querySelector('#mw-content-text > p').innerText;
-    // let tbody = document.querySelector('#mw-content-text > table.infobox.box.colored.bordered.innerbordered.fill-td.type-character.list-noicon.float-right-clear').querySelector('tbody');
-    // for (let row of tbody.children) {
-    //   if (row.querySelector('th')) {
-    //     console.log(row.querySelector('th').innerText);
-    //     }
-    // }
-    return {name, summary};
-  })
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
+// grab all links to characters
+let characterLinks = fs.readFileSync("characterLinks.json", "utf8");
+characterLinks = JSON.parse(characterLinks);
+
+// bring characters in from current json file
+let characters = fs.readFileSync('characters.json', 'utf8');
+characters = JSON.parse(characters);
+
+async function pageScraper(page) {
+  let results = await page.evaluate(async () => {
+  let obj = {};
+  let name = document.querySelector("#PageHeader > div.page-header__main > h1").innerText;
+  let summary = document.querySelector("#mw-content-text > p").innerText;
+  obj['name'] = name;
+  obj['summary'] = summary;
+  let tbody = document.querySelector("#mw-content-text > table.infobox.box.colored.bordered.innerbordered.fill-td.type-character.list-noicon.float-right-clear").querySelector("tbody");
+  for (let row of tbody.children) {
+    if (row.querySelector("th")) {
+      if (row.querySelector("td")) {
+        let th = row.querySelector("th");
+        let td = row.querySelector("td");
+        let attr = await window.attributeMaker(th.innerText);
+        let value = td.innerText.trim();
+        obj[attr] = value;
+      }
+    }
+  }
+  return obj;
+  })
   return results;
 }
 
-// let results = {};
-// let name = document.querySelector('#PageHeader > div.page-header__main > h1').innerText;
-// let summary = document.querySelector('#mw-content-text > p').innerText;
-// results['name'] = name;
-// results['summary'] = summary;
-// let tbody = document.querySelector('#mw-content-text > table.infobox.box.colored.bordered.innerbordered.fill-td.type-character.list-noicon.float-right-clear').querySelector('tbody');
-// for (let row of tbody.children) {
-//   if (row.querySelector('th')) {
-//     let th = row.querySelector('th')
-//     if (row.querySelector('td')) {
-//       td = row.querySelector('td');
-//       let attr = attributeMaker(th.innerText);
-//       let value = td.innerText.trim();
-//       results[attr] = value;
-//     }
-//   }
-// }
+async function scrape() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-// function attributeMaker(string) {
-//   string = string.split(" ");
-//   if (string.length === 1) return string[0].toLowerCase();
-//   for (let i = 0; i < string.length; i++) {
-//       if (i === 0) {
-//           string[i] = string[i].toLowerCase();
-//       } else {
-//           string[i] = string[i][0].toUpperCase() + string[i].slice(1);
-//       }
-//   }
-//   return string.join("");
-// }
+  await page.exposeFunction('attributeMaker', (text) => {
+    text = text.split(" ");
 
-  // traverse each character link and grab all da data!
-  // for (characterLink of characterLinks) {
-  //   await page.goto(characterLink);
-  //   const results = await getCharacterInfoFrom(page);
-  //   new Character(results)
+    if (text.length === 1) return text[0].toLowerCase();
+
+    for (let i = 0; i < text.length; i++) {
+      if (i === 0) {
+        text[i] = text[i].toLowerCase();
+      } else {
+        text[i] = text[i][0].toUpperCase() + text[i].slice(1);
+      }
+    }
+    return text.join("");
+  })
+
+  // for (let link of characterLinks) {
+  //   await page.goto(link, {waitUntil: 'networkidle2'});
+  //   let results = await pageScraper(page);
+  //   characters.push(results);
+  //   let json = JSON.stringify(characters);
+  //   fs.writeFileSync('characters.json', json);
   // }
+
+  for (let i = characterLinks.length - 3; i < characterLinks.length; i++) {
+    let link = characterLinks[i];
+    await page.goto(link, {waitUntil: 'networkidle2'});
+    let results = await pageScraper(page);
+    characters.push(results);
+    let json = JSON.stringify(characters);
+    fs.writeFileSync('characters.json', json);
+  }
+
+  browser.close();
+  let json = JSON.stringify(characters);
+  fs.writeFileSync('characters.json', json);
+}
+
+scrape();
